137646cedb1b72a1fab8a141a6dea3477c71c395
bagoesrex <bagoesrex@gmail.com>
2025-07-06 04:30:21 +0700

    feat: setup laravel sanctum for API authentication

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::group(['namespace' => 'App\Http\Controllers\API'], function () {
+    // --------------- Register and Login ----------------//
+    Route::post('register', 'AuthenticationController@register')->name('register');
+    Route::post('login', 'AuthenticationController@login')->name('login');
+    
+    // ------------------ Get Data ----------------------//
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('get-user', 'AuthenticationController@userInfo')->name('get-user');
+        Route::post('logout', 'AuthenticationController@logOut')->name('logout');
+    });
+});
